f5122f2fd2fa7b5f93f90dfaef69ae2ceb3224ce
Francisco <fmartinez@weedmaps.com>
2019-09-19 22:17:44 -0600

    add images

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, Button } from 'react-native';
-
+import GallerySwiper from "react-native-gallery-swiper";
 
 const styles = StyleSheet.create({
   container: {
@@ -51,15 +51,62 @@ class GalleryScreen extends React.Component {
         title="Home"
         onPress={() => navigate('Home', {name: 'GalleryScreen'})}
       />
+            <Button
+        title="Bebidas"
+        onPress={() => navigate('Bebidas', {name: 'GalleryScreen'})}
+      />
+
       </View>
     );
   }
 }
 
 
+
+class BebidasScreen extends React.Component {
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <View style={styles.container}>
+
+<GallerySwiper
+            images={[
+                { source: require("../rinconcitomobile/img/bananasconcrema.jpg"),
+                   width: 1080,
+                   height: 1920 },
+                { source: require("../rinconcitomobile/img/bananasplit_2.jpg"),
+                    width: 1080,
+                    height: 1920 },
+                { source: require("../rinconcitomobile/img/clamatoreparado.jpg"),
+                    width: 1080,
+                    height: 1920 },
+                { source: require("../rinconcitomobile/img/dorinachos_2.jpg"),
+                    width: 1080,
+                    height: 1920 },
+                    { source: require("../rinconcitomobile/img/bananasconcrema.jpg"),
+                    width: 1080,
+                    height: 1920 },
+                 { source: require("../rinconcitomobile/img/bananasplit_2.jpg"),
+                     width: 1080,
+                     height: 1920 },
+                 { source: require("../rinconcitomobile/img/clamatoreparado.jpg"),
+                     width: 1080,
+                     height: 1920 },
+                 { source: require("../rinconcitomobile/img/dorinachos_2.jpg"),
+                     width: 1080,
+                     height: 1920 },
+            ]}
+        />
+
+      </View>
+    );
+  }
+}
+
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
   Gallery: {screen: GalleryScreen},
+  Bebidas: {screen: BebidasScreen}
 
 });
 
